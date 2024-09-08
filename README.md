ebd856fd50904d5403f19c759ed352fe97772f11
nickvsnetworking <nick@nickvsnetworking.com>
2024-09-08 19:57:16 +1000

    Deploy

--- README.md
+++ README.md
@@ -1,71 +1,35 @@
-# Getting Started with Create React App
+# Omnitouch CGrateS React UI
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is a simple UI to access the CGrateS API, created by Omnitouch Network Services.
 
-## Available Scripts
+It runs in the browser and connects to any CGrateS instance the computer you're reading this on can see, including localhost.
 
-In the project directory, you can run:
+## Setup
 
-### `npm start`
+By default this tool tries to connect to a CGrateS instance running on `localhost:2080` but you can change this to point at other instances.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+In order to use this tool your version of CGrateS must be later than [this commit](https://github.com/cgrates/cgrates/pull/4430/commits/1b6942397ee7e7211d0d597dba65b2e9721782f1) which adds support for CORS to React can talk to CGrateS.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## Usage
 
-### `npm test`
+Only certain functions / endpoints are exposed via this tool, it's in no way comprehensive.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+This of it less of a "GUI for CGrateS" and more of a utility for managing data inside CGrateS.
 
-### `npm run build`
+If you're new to CGrateS, and want to learn more about it:
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+ * [CGrateS Documentation](https://cgrates.readthedocs.io/en/latest/)
+ * [ITsysCom (Developers of CGrateS)](support@itsyscom.com)
+ * [Mailing List](https://groups.google.com/g/cgrates)
+ * [CGrateS in Baby Steps Tutorial Series](https://nickvsnetworking.com/category/voip/cgrates/)
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+## How do I...
+Not all API endpoints are exposed here.
+So far what has been include is limited support for:
+ * Searching and exporting CDRs
+ * Viewing Rates / Least Cost Routes
+ * Viewing AttributeS / Testing AttributeS
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
-# CGrateS_UI
+## Contributions
+Contributions welcome via the Github page.
--- src/App.js
+++ src/App.js
@@ -5,53 +5,46 @@ import CDRs from './CDRs';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
-  const [cgrates_URL, setcgrates_URL] = useState('');
-  const [cgrates_Tenants, setcgrates_Tenants] = useState('');
-  const [cgrates_Username, setcgrates_Username] = useState('');
-  const [cgrates_Password, setcgrates_Password] = useState('');
+  const [cgratesConfig, setCgratesConfig] = useState({
+    url: 'http://localhost:2080',
+    tenants: 'cgrates.org;test',
+    username: '',
+    password: ''
+  });
   const [showModal, setShowModal] = useState(false);
 
-  // Check for the API key and API user in local storage
+  // Check for the CGrateS configuration in local storage
   useEffect(() => {
-    const storedcgrates_URL = localStorage.getItem('cgrates_URL');
-    if (storedcgrates_URL) {
-      setcgrates_URL(storedcgrates_URL);
+    const storedConfig = localStorage.getItem('cgratesConfig');
+    if (storedConfig) {
+      setCgratesConfig(JSON.parse(storedConfig));
     } else {
-      setShowModal(true); // Show modal if no API key or API user is found
+      setShowModal(true); // Show modal if no config is found
     }
   }, []);
 
-  // Update the API key and API user in local storage whenever they change
+  // Update the CGrateS configuration in local storage whenever it changes
   useEffect(() => {
-    if (cgrates_URL) {
-      localStorage.setItem('cgrates_URL', cgrates_URL);
+    if (cgratesConfig.url) {
+      localStorage.setItem('cgratesConfig', JSON.stringify(cgratesConfig));
     }
-  }, [cgrates_URL]);
+  }, [cgratesConfig]);
 
-  // Handle API key change from the modal input
-  const handlecgrates_URLChange = (event) => {
-    setcgrates_URL(event.target.value);
+  // Handle input changes
+  const handleConfigChange = (key, value) => {
+    setCgratesConfig((prevConfig) => ({
+      ...prevConfig,
+      [key]: value
+    }));
   };
 
-  // Handle CGrateS Tenants change from the modal input
-  const handlecgrates_TenantsChange = (event) => {
-    setcgrates_Tenants(event.target.value);
-  };
-
-  // Handle API user change from the modal input
-  const handlecgrates_UsernameChange = (event) => {
-    setcgrates_Username(event.target.value);
-  };
-
-  // Handle API password change from the modal input
-  const handlecgrates_Password = (event) => {
-    setcgrates_Password(event.target.value);
-  };
-
-  // Handle opening the modal to change the API key
+  // Handle opening the modal to change the CGrateS config
   const handleOpenModal = () => {
     setShowModal(true);
   };
+
+  // Split tenants by ';' and give hint in the UI
+  const splitTenants = cgratesConfig.tenants.split(';');
 
   return (
     <Router basename="/">
@@ -61,7 +54,7 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link} to="/sms-request">View CDRs</Nav.Link>
+              <Nav.Link as={Link} to="/cdrs">View CDRs</Nav.Link>
             </Nav>
             <Button variant="outline-info" onClick={handleOpenModal}>Link to CGrateS</Button>
           </Navbar.Collapse>
@@ -70,7 +63,7 @@ function App() {
 
       <Container>
         <Routes>
-          <Route path="/sms-request" element={<CDRs cgrates_URL={cgrates_URL} />} />
+          <Route path="/cdrs" element={<CDRs cgratesConfig={cgratesConfig} />} />
         </Routes>
       </Container>
 
@@ -81,50 +74,62 @@ function App() {
         <Modal.Body>
           <Form>
             <Form.Group>
-              <Form.Label>API Key</Form.Label>
+              <Form.Label>API URL</Form.Label>
               <Form.Control
                 type="text"
                 defaultValue="http://localhost:2080"
-                value={cgrates_URL}
-                onChange={handlecgrates_URLChange}
+                value={cgratesConfig.url}
+                onChange={(e) => handleConfigChange('url', e.target.value)}
               />
+              <Form.Text className="text-muted">
+                URL of CGrateS instance (default is <pre>http://localhost:2080</pre>
+              </Form.Text>
             </Form.Group>
             <Form.Group>
-              <Form.Label>Tenants</Form.Label>
+              <Form.Label>Tenants (separate by ;)</Form.Label>
               <Form.Control
                 type="text"
                 defaultValue="cgrates.org;test"
-                value={cgrates_Tenants}
-                onChange={handlecgrates_TenantsChange}
+                value={cgratesConfig.tenants}
+                onChange={(e) => handleConfigChange('tenants', e.target.value)}
               />
+              <Form.Text className="text-muted">
+                Please enter tenants separated by semicolon (;)
+              </Form.Text>
             </Form.Group>
             <Form.Group>
               <Form.Label>Basic Auth - Username</Form.Label>
               <Form.Control
                 type="text"
-                defaultValue=""
-                value={cgrates_Username}
-                onChange={handlecgrates_UsernameChange}
+                value={cgratesConfig.username}
+                onChange={(e) => handleConfigChange('username', e.target.value)}
               />
+              <Form.Text className="text-muted">
+                Optional - only required if CGrateS is configured with basic auth
+              </Form.Text>
             </Form.Group>
             <Form.Group>
               <Form.Label>Basic Auth - Password</Form.Label>
               <Form.Control
-                type="text"
-                defaultValue=""
-                value={cgrates_Password}
-                onChange={handlecgrates_Password}
+                type="password"
+                value={cgratesConfig.password}
+                onChange={(e) => handleConfigChange('password', e.target.value)}
               />
+              <Form.Text className="text-muted">
+                Optional - only required if CGrateS is configured with basic auth
+              </Form.Text>
             </Form.Group>
           </Form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={() => setShowModal(false)}>Close</Button>
-          <Button variant="primary" onClick={() => { setcgrates_URL(cgrates_URL); setShowModal(false); }}>Save Changes</Button>
+          <Button variant="primary" onClick={() => setShowModal(false)}>Save Changes</Button>
         </Modal.Footer>
       </Modal>
+
       <small>
-        Currently using API key: {cgrates_URL}
+        Currently using CGrateS URL: <pre>{cgratesConfig.url}</pre>
+        Tenants: <pre>{splitTenants.join(', ')}</pre>
       </small>
     </Router>
   );
